78fd2c54f44c6d32921e4996cff5aa9c883f9d3a
Christian Oliveira Leichtweis <chris99leichtweiss@gmail.com>
2021-11-27 11:53:03 -0300

    Aula 1

---
+++ aula 1 - semântica pt1/index.js
@@ -0,0 +1,20 @@
+Semântica
+
+<section> 
+Sessão Genérica de Conteúdo (lista de artigos)
+
+<aside>
+Representa um conteúdo relacionado ao conteúdo principal da página 
+(barra lateral, biografia de um autor, links relacionados)
+
+<header>  
+Cabeçalho da página ou de parte da página
+
+<footer>
+Rodapé da página ou debaixo da página (rodapé de um article ou um section)
+
+<article>
+Conteúdo relevante (artigo de um blog)      
+
+<h1>-<h6>
+Títulos
